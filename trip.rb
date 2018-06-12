b291b4b6d037ef3c3e5690c7c73d9f154d5e93ad
salespinosa <sal@turing.io>
2018-06-12 13:43:09 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -9,8 +9,14 @@ stay = gets.chomp.capitalize  #don't forget to Capitilize the input
 
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 
+puts "What would you like to eat?"
 
+eat = gets.chomp
+
+puts "How many nights would you like to stay?"
+
+nights = gets.chomp 
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts ""
+puts "Cool! You're going to #{stay} for #{nights} nights and you're going to eat #{eat}."
